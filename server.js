6b43e22b782966c113f684f6dfff21b321beb49b
LRett09 <loganoliverrettich@gmail.com>
2025-12-02 08:58:48 -0800

    Update server.js

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.post('/', (req, res) => {
             <title>Browser Title</title>
         </head>
         <body>
-            <h3 align=center>t2a27-render-node</h3>
+            <h3 align=center>T2A06-node-codesandbox</h3>
             <form action="/" method="post">
                 <label for="myText01">Enter Text:</label>
                 <input type="text" id="myText01" name="myText01">
